ddf785a9d7533815130ad5c1509db118809aceb0
Dhaniele Demontano <dhane1000@gmail.com>
2022-06-12 18:35:01 +0800

    added font-awesome assets and a few css files. ongoing front-page about me

---
+++ my-portfolio/wwwroot/js/fullscreen.js
@@ -0,0 +1,20 @@
+var elem = document.documentElement;
+function openFullScreen() {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+  }
+
+  function closeFullScreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+  }
